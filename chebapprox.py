0822aa784465186244f70cb1bacd725474ea6dcf
Brian Leahy <bdl48@cornell.edu>
2019-09-02 10:45:45 -0400

    working 1d cheb approximant with tests, some missing tests though

---
+++ chebapprox.py
@@ -0,0 +1,96 @@
+import numpy as np
+from numpy.polynomial.chebyshev import chebval
+
+
+class ChebyshevApproximant(object):
+    def __init__(self, function, window, degree=6, nevalpts=None,
+                 function_args=(), function_kwargs={}):
+        """A 1D Chebyshev approximation / interpolation for an ND function,
+        approximating (N-1)D in in the last dimension.
+
+        Parameters
+        ----------
+        func : callable
+            A function that takes scalar arguments (1D) and returns a N
+            dimensional array corresponding to that scalar. Make it such that,
+            for an array x, f(x)[.....,a] corresponds to f(x[a])
+
+        args : tuple [optional]
+            extra arguments to pass to func
+
+        window : tuple (length 2)
+            The bounds of the function over which we desire the interpolation
+
+        degree : integer
+            Degree of the Chebyshev interpolating polynomial
+
+        evalpts : integer
+            Number of Chebyshev points to evaluate the function at
+
+        Examples
+        --------
+        >>> import numpy as np
+        >>> func = lambda x: np.sin(x)
+        >>> cheb = ChebyshevInterpolation1D(func, window=(0, np.pi), degree=9,
+        ...                                 evalpts=11)
+        >>> np.allclose(cheb(1.0), np.sin(1.0), atol=1e-7)
+        True
+        """
+        self.function = function
+        self.window = tuple(window)
+        self.degree = int(degree)
+        if nevalpts is None:
+            nevalpts = self.degree + 3
+        self.nevalpts = nevalpts
+        self.function_args = function_args  # FIXME test
+        self.function_kwargs = function_kwargs  # FIXME test
+
+        if self.nevalpts < self.degree:
+            raise ValueError("nevalpts must be > degree")
+
+        self._coeffs = self._construct_coefficients()
+
+    def __call__(self, x):
+        chebx = self._transform_raw_to_cheb_coordinates(x)
+        return chebval(chebx, self._coeffs, tensor=True)
+
+    @property
+    def coefficients(self):
+        return self._coeffs.copy()
+
+    def evaluate_cheb_tk(self, k, rawx):  # FIXME test
+        chebx = self._transform_raw_to_cheb_coordinates(rawx)
+        weights = np.zeros(k + 1)
+        weights[k] = 1.0
+        return np.polynomial.chebyshev.chebval(chebx, weights)
+
+    def _transform_raw_to_cheb_coordinates(self, rawx):
+        chebx = 2 * (rawx - min(self.window)) / np.ptp(self.window)
+        chebx -= 1.0
+        return chebx
+
+    def _transform_cheb_to_raw_coordinates(self, chebx):
+        rawx = ((chebx + 1) * (self.window[1] - self.window[0]) * 0.5 +
+                self.window[0])
+        return rawx
+
+    def _construct_coefficients(self):
+        # TODO: make this faster once you test it for ND functions
+        coeffs = np.zeros(self.degree)
+        N = float(self.nevalpts)
+
+        lvals = np.arange(self.nevalpts).astype('float')
+        xpts = self._transform_cheb_to_raw_coordinates(
+            np.cos(np.pi * (lvals + 0.5) / N))
+        fpts = np.rollaxis(
+            self.function(xpts, *self.function_args, **self.function_kwargs),
+            -1)
+
+        for a in range(self.degree):
+            for b in range(self.nevalpts):
+                coeffs[a] += fpts[b] * np.cos(np.pi*a*(lvals[b]+0.5)/N)
+        coeffs *= 2.0 / N
+        coeffs[0] *= 0.5
+        return np.array(coeffs)
+
+
---
+++ test_chebapprox.py
@@ -0,0 +1,91 @@
+import unittest
+
+import numpy as np
+from numpy.polynomial.chebyshev import chebval
+
+from chebapprox import ChebyshevApproximant
+
+
+TOLS = {'atol': 1e-14, 'rtol': 1e-14}
+
+
+class TestChebApproximant(unittest.TestCase):
+    def test_stores_init_kwargs(self):
+        window = (0, 1.0)
+        function = np.sin
+        degree = 8
+        nevalpts = 11
+
+        cheb =ChebyshevApproximant(
+            function, window, degree=degree, nevalpts=nevalpts)
+        self.assertTrue(cheb.function is function)
+        self.assertEqual(cheb.window, tuple(window))
+        self.assertEqual(cheb.degree, degree)
+        self.assertEqual(cheb.nevalpts, nevalpts)
+
+    def test_default_nevalpts(self):
+        window = (0, 1.0)
+        function = np.sin
+        degree = 8
+
+        cheb =ChebyshevApproximant(
+            function, window, degree=degree, nevalpts=None)
+        self.assertEqual(cheb.nevalpts, cheb.degree + 3)
+
+    def test_raises_error_if_nevalpts_lessthan_degree(self):
+        self.assertRaises(
+            ValueError, ChebyshevApproximant, np.sin, (0, 1), degree=6,
+            nevalpts=4)
+
+    def test_transform_raw_to_cheb_coordinates(self):
+        cheb = make_cheb_interpolant()
+        rawx = generate_random_points_on_window(cheb.window)
+        chebx = cheb._transform_raw_to_cheb_coordinates(rawx)
+        self.assertTrue(np.all(chebx <= 1))
+        self.assertTrue(np.all(chebx >= -1))
+        rescale = 0.5 * np.ptp(cheb.window)
+        self.assertAlmostEqual(rawx.ptp(), chebx.ptp() * rescale, places=14)
+
+    def test_transform_cheb_to_raw_coordinates(self):
+        cheb = make_cheb_interpolant()
+        rawx = generate_random_points_on_window(cheb.window)
+        chebx = cheb._transform_raw_to_cheb_coordinates(rawx)
+        backagain = cheb._transform_cheb_to_raw_coordinates(chebx)
+        self.assertTrue(np.allclose(rawx, backagain, **TOLS))
+
+    def test_construct_coefficients(self):
+        np.random.seed(10)
+        degree = 6
+        true_coeffs = np.random.randn(degree)
+        f = lambda x: chebval(x, true_coeffs)
+        cheb = ChebyshevApproximant(f, window=(-1, 1), degree=degree)
+        constructed_coeffs = cheb._construct_coefficients()
+        self.assertTrue(np.allclose(true_coeffs, constructed_coeffs, **TOLS))
+
+    def test_coefficients_returns_copy(self):
+        cheb = make_cheb_interpolant()
+        self.assertTrue(cheb._coeffs is not cheb.coefficients)
+        self.assertTrue(np.all(cheb._coeffs == cheb.coefficients))
+
+    def test_call_accurately_approximates(self):
+        cheb = ChebyshevApproximant(function=np.sin, window=(0, 1), degree=12)
+        x = np.linspace(0, 1, 101)
+        true = np.sin(x)
+        approx = cheb(x)
+        self.assertTrue(np.allclose(true, approx, **TOLS))
+
+
+
+def make_cheb_interpolant(function=np.sin):
+    cheb = ChebyshevApproximant(function, (0, 1.0), degree=8)
+    return cheb
+
+
+def generate_random_points_on_window(window, npts=100, seed=72):
+    np.random.seed(seed)
+    return min(window) + np.ptp(window) * np.random.rand(npts)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
